843b5435423f17392381930b7048f0ff3185eedf
Pol Pavo <pol.p.c@hotmail.com>
2025-03-11 15:50:59 +0100

    3 leds serie

--- P3_serie_LED/P3_serie_LED.ino
+++ P3_serie_LED/P3_serie_LED.ino
@@ -1,9 +1,13 @@
-int LED = 13;
+int LED1 = 13;
+int LED2 = 3;
+int LED3 = 4;
 int data;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED, OUTPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -12,10 +16,18 @@ void loop() {
   if(Serial.available()){
     data = Serial.read();
     if(data == '1'){
-       digitalWrite(LED, HIGH);
+       digitalWrite(LED1, HIGH);
      }
+    if(data == '2'){
+      digitalWrite(LED2, HIGH);
+      }
+    if(data == '3'){
+      digitalWrite(LED3, HIGH);
+      }
     if(data == '0'){
-      digitalWrite(LED, LOW);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, LOW);
       } 
    }
 }
